cb90cd187012f67eb7a2784909294714b24210fe
TerenceGrover <terencegrover@gmail.com>
2024-02-01 13:59:44 +0100

    rename + remove randomness

--- Final_render.py
+++ Final_render.py
@@ -16,14 +16,14 @@ last_value = 100
 last_significant_change_time = time.time()
 melody_freq = Sig(value=100)
 harmony_freq = Sig(value=100)
-mul_obj = Sig(0.6)
+mul_obj = Sig(0.85)
 lfo = Sine(freq=0.2).range(1000, 5000)
 major_scale = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25]
 minor_scale = [261.63, 293.66, 311.13, 349.23, 392.00, 415.30, 466.16, 523.25]
-threshold = 50
+threshold = 5
 env = Adsr(attack=0.6, decay=0.2, sustain=0.3, release=0.5, mul=mul_obj).play()
 
-arduino = asr.init_arduino('/dev/cu.usbmodem11201')
+arduino = asr.init_arduino('/dev/cu.usbmodem1201')
 # SENSOR READING FUNCTIONS
 
 
@@ -69,15 +69,14 @@ def read_value_thread():
     global reverb_size_melo
     global reverb_size_harmo
     light_value = read_value()['light']
-    soil_moisture = read_soil_moisture()
+    soil_moisture = read_value()['soil']
     scale = get_scale(light_value)
     counter = 0
 
     while True:
         current_value = read_value()
-        # TO REMOVE
-        # current_value = current_value + random.randint(-2, 2)
-        last_value = harmony_freq.get()
+        # TO REMOVE | TEST ONLY
+        # current_value['electrode'] += random.randint(0, 8)
 
         if counter % random.randint(5, 10) == 0:
             mapped_value = map_adc_to_scale(current_value['electrode'], scale)
@@ -87,11 +86,12 @@ def read_value_thread():
         # Update the freq_obj's value to change the frequency of the sine waves
         # Shift melody one octave higher
             melody_freq.setValue(shift_octave(mapped_value, 0))
-            if light_value > 50:
-                melody_freq.setValue(shift_octave(mapped_value, 1))
 
         if counter % 20 == 0:
-            harmony_freq.setValue(shift_octave(mapped_value, -1))
+            if mapped_value < 1900:
+                harmony_freq.setValue(shift_octave(mapped_value, 1))
+            else:
+                harmony_freq.setValue(shift_octave(mapped_value, -1))
 
         chorus_depth_harmo = map_value(harmony_freq, 100, 900, 0.4, 1)
         chorus_depth_melo = map_value(melody_freq, 100, 900, 0.1, 0.6)
@@ -104,8 +104,10 @@ def read_value_thread():
             f"ADC Value: {current_value['electrode']}, Mapped Frequency: {mapped_value}")
 
         counter += 1
+        if counter % 10 == 0:
+            last_value = melody_freq.get()
 
-        time.sleep((-(current_value['electrode']/500)) ** current_value['electrode'] /
+        time.sleep((current_value['electrode']/2000) ** current_value['electrode'] /
                    50 + random.uniform(0.05, 0.2) + 0.5)
 
 
@@ -115,12 +117,11 @@ def check_freq_change():
     print('melody_freq', melody_freq.get())
     print('last_value', last_value)
     diff = abs(melody_freq.get() - last_value)
-    print('diff', diff)
     if diff > threshold:
         mul_obj.setValue(0.9)
         env.play()
         last_significant_change_time = time.time()
-    elif time.time() - last_significant_change_time > 3:  # 3 seconds without significant change
+    elif time.time() - last_significant_change_time > 5:  # 3 seconds without significant change
         current_mul = mul_obj.value
         # Decrease the amplitude by 10% every check
         mul_obj.setValue(current_mul * 0.9)
@@ -128,7 +129,7 @@ def check_freq_change():
 
 # MAIN LOGIC AND SYNTHESIS
 # Use a Metro to regularly check the difference
-metro = Metro(time=read_value()['temperature']/100).play()
+metro = Metro(time=read_value()['temperature']/50).play()
 trig_func = TrigFunc(metro, function=check_freq_change)
 if read_value()['humidity'] < 50:
     melody_synth = SineLoop(freq=melody_freq, mul=env)
@@ -152,7 +153,7 @@ chorus_melo = Chorus(harmonizer_melo,
 chorus_harmo = Chorus(harmonizer_harmo,
                       depth=chorus_depth_harmo, bal=0.5)
 # A reverb to add space
-reverb_melo = Freeverb(chorus_melo, size=reverb_size_melo, damp=0.4, bal=0.5)
+reverb_melo = Freeverb(chorus_melo, size=reverb_size_melo, damp=0.4, bal=0.5).out()
 reverb_harmo = Freeverb(
     chorus_harmo, size=reverb_size_harmo, damp=0.2, bal=0.5)
 # A delay for cascading echoes
@@ -160,7 +161,6 @@ delay_melo = SmoothDelay(reverb_melo, delay=0.5,
                          feedback=0.05, maxdelay=1).out()
 delay_harmo = SmoothDelay(reverb_harmo, delay=0.6, feedback=0.3, maxdelay=2)
 # A slow-moving low-pass filter for a sweeping effect
-# lfo = Sine(freq=0.2).range(1000, 5000)
 lpf = ButLP(delay_harmo, freq=lfo).out()
 
 # Display
